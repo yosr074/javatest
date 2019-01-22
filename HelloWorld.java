0e2d1f0d80b5cd5d86f24e6779791e8f8e948e0e
root <yosr.elyounsi@gmail.com>
2019-01-22 15:09:42 -0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class HelloWorld {
+  // Définition de la fonction main
+  public static void main(String[] arg){
+    // Affichage du commentaire Hello world dans la fenêtre de commande
+    System.out.println("Hello world");
+}
+}
+  
+
